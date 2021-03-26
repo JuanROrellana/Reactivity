654808ade60cb7cae06a1e37c3638533d2ba3be4
Juan Jose Ramirez <ramirez.orellana.juanjose@gmail.com>
2021-03-25 23:54:21 -0600

    Add list with mediator

---
+++ Api/Controllers/ActivitiesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Persistence;
+using Application.Activities;
+using Domain;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ActivitiesController : BaseApiController
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> Get()
+        {
+            return await Mediator.Send(new List.Query());
+        }
+    }
+}
