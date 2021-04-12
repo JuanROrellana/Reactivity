8969d570f18a8a85810c8a785438af50bb448f64
Juan Jose Ramirez <ramirez.orellana.juanjose@gmail.com>
2021-04-11 20:46:10 -0600

    Add Infinity scroll

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -18,6 +18,7 @@ import {useStore} from "../stores/store";
 import LoadingComponent from "./LoadingComponent";
 import ModalContainer from '../common/modals/ModalContainer';
 import ProfilePage from "../../features/profiles/ProfilePage";
+import PrivateRoute from "./PrivateRoute";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -55,11 +56,11 @@ function App() {
                            <Container fixed>
                                <div className={classes.root}>
                                    <Switch>
-                                       <Route exact path='/activities' component={ActivityDashboard}/>
-                                       <Route path='/activities/:id' component={ActivityDetails}/>
-                                       <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
-                                       <Route path='/profiles/:userName' component={ProfilePage}/>
-                                       <Route path='/errors' component={TestErrors}/>
+                                       <PrivateRoute exact path='/activities' component={ActivityDashboard}/>
+                                       <PrivateRoute path='/activities/:id' component={ActivityDetails}/>
+                                       <PrivateRoute key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
+                                       <PrivateRoute path='/profiles/:userName' component={ProfilePage}/>
+                                       <PrivateRoute path='/errors' component={TestErrors}/>
                                        <Route path='/server-error' component={ServerError} />
                                        <Route path='/login' component={LoginForm} />
                                        <Route component={NotFound}/>
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -10,12 +10,14 @@ import { StoreContext, store } from "./app/stores/store";
 import reportWebVitals from './reportWebVitals'
 import { Router } from 'react-router-dom';
 import {createBrowserHistory} from 'history';
+import ScrollToTop from "./app/layout/ScrollToTop";
 
 export const history = createBrowserHistory();
 
 ReactDOM.render(
     <StoreContext.Provider value={store}>
         <Router history={history}>
+            <ScrollToTop/>
             <App />
         </Router>
     </StoreContext.Provider>,
